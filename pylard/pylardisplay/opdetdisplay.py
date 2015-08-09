28fae88fb1bc4fd82103003eb3cc808636ac45bb
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-08-09 17:20:36 -0400

    added pmt position indicator for opdetdisplay

--- pylard/pylardisplay/opdetdisplay.py
+++ pylard/pylardisplay/opdetdisplay.py
@@ -5,16 +5,31 @@ import pyqtgraph as pg
 import numpy as np
 from collections import OrderedDict
 
+from pylard.config.pmt_chmap import getPMTID, getChannel, getPMTIDList, getPaddleIDList
+from pylard.config.pmtpos import getPosFromID 
+
 class OpDetDisplay(QtGui.QWidget) :
     def __init__(self, opdata):
         super(OpDetDisplay,self).__init__()
         self.opdata = opdata
-        self.plot = pg.PlotWidget(name='Plot1')
-        self.layout = QtGui.QGridLayout()
-        self.setLayout(self.layout)
+
+        # Plots
+        self.graphics = pg.GraphicsLayoutWidget()
+        self.plot = pg.PlotItem(name='Plot1')
+        #self.diagram = pg.ViewBox()
+        self.diagram = pg.PlotItem(name="plot2")
+        self.pmtscale =  pg.GradientEditorItem(orientation='bottom')
+
 
         # main layout
-        self.layout.addWidget( self.plot, 0, 0 )
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+        #self.layout.addWidget( self.diagram, 0, 0 )
+        #self.layout.addWidget( self.plot, 1, 0 )
+        self.layout.addWidget( self.graphics, 0, 0 )
+        self.graphics.addItem( self.diagram, 0, 0 )
+        self.graphics.addItem( self.pmtscale, 1, 0 )
+        self.graphics.addItem( self.plot, 2, 0 )
         self.lay_inputs = QtGui.QGridLayout()
         self.layout.addLayout( self.lay_inputs, 1, 0 )
         
@@ -30,7 +45,7 @@ class OpDetDisplay(QtGui.QWidget) :
         self.start_sample = QtGui.QLineEdit("0")
         self.end_frame  =  QtGui.QLineEdit("%d"%(self.first_frame))
         self.end_sample = QtGui.QLineEdit("1000")
-        self.set_xaxis = QtGui.QPushButton("Set X-axis")
+        self.set_xaxis = QtGui.QPushButton("Plot!")
 
         self.lay_inputs.addWidget( QtGui.QLabel("Event"), 0, 0 )
         self.lay_inputs.addWidget( self.event, 0, 1 )
@@ -48,6 +63,13 @@ class OpDetDisplay(QtGui.QWidget) :
         self.lay_inputs.addWidget( QtGui.QLabel("Sample"), 0, 12 )
         self.lay_inputs.addWidget( self.end_sample, 0, 13 )
         self.lay_inputs.addWidget( self.set_xaxis, 0, 14 )
+
+        # range selections
+        self.time_range = pg.LinearRegionItem(values=[50,150], orientation=pg.LinearRegionItem.Vertical)
+        self.plot.addItem( self.time_range )
+
+        # diagram objects
+        self.definePMTdiagram()
 
         # connect
         self.set_xaxis.clicked.connect( self.plotData )
@@ -78,9 +100,57 @@ class OpDetDisplay(QtGui.QWidget) :
         xmin = (sframe-self.first_frame)*self.ticsperframe + ssample
         xmax = (eframe-self.first_frame)*self.ticsperframe + esample
 
-        self.plot.getPlotItem().clear()
+        self.plot.clear()
         for ipmt in xrange(0,self.opdata.opdetdigi.shape[1]):
-            self.plot.plot( self.opdata.opdetdigi[:,ipmt]-2045.0+ipmt*1000, pen=(255,255,255), name="PMT%d"%(ipmt))
+            if ipmt in getPMTIDList():
+                # PMT
+                self.plot.plot( self.opdata.opdetdigi[:,ipmt]-2045.0+ipmt*1000, pen=(255,255,255), name="PMT%d"%(ipmt))
+            elif ipmt in getPaddleIDList():
+                # PADDLE
+                self.plot.plot( self.opdata.opdetdigi[:,ipmt]-2045.0+ipmt*1000, pen=(0,0,255), name="PMT%d"%(ipmt))
+            else:
+                # LOGIC
+                self.plot.plot( self.opdata.opdetdigi[:,ipmt]-2045.0+ipmt*1000, pen=(0,255,0), name="PMT%d"%(ipmt))
         self.plot.setXRange(xmin,xmax,update=True)
+        self.plot.addItem( self.time_range )
+
+        # ----------------------------------------------------
+        # diagram object
+        bnds = self.time_range.getRegion()
+        if bnds[0]>bnds[1]:
+            tmp = bnds[0]
+            bnds[0] = bnds[1]
+            bnds[1] = tmp
+
+        self.pmtspot = []
+        for ich in xrange(0,self.opdata.opdetdigi.shape[1]):
+            if ich>=36:
+                continue
+            maxamp = np.max( self.opdata.opdetdigi[bnds[0]:bnds[1],ich] )-2048.0
+            ipmt = getPMTID( ich )-1
+            #print "maxamp: id=",ipmt,' max=',maxamp
+            col = self.pmtscale.colorMap().map( (maxamp)/2048.0 )
+            if ipmt in getPMTIDList():
+                pos = getPosFromID(ipmt )
+                self.pmtspot.append( {"pos":(pos[2],pos[1]), "size":30, 'pen':{'color':'w','width':2}, 'brush':col, 'symbol':'o'} )
+            elif ipmt in getPaddleIDList():
+                pos = getPosFromID( ipmt )
+                self.pmtspot.append( {"pos":(pos[2],pos[1]), "size":25, 'pen':{'color':(0,0,255),'width':2}, 'brush':col, 'symbol':'s'} )
+        self.pmtdiagram.setData( self.pmtspot )
+        
 
 
+    def definePMTdiagram(self):
+        self.pmtspot = []
+        for pid in getPMTIDList():
+            pos = getPosFromID( pid )
+            self.pmtspot.append( {"pos":(pos[2],pos[1]), "size":30, 'pen':{'color':'w','width':2}, 'brush':(255,255,255,255), 'symbol':'o'} )
+        for pid in getPaddleIDList():
+            pos = getPosFromID( pid )
+            self.pmtspot.append( {"pos":(pos[2],pos[1]), "size":25, 'pen':{'color':(0,0,255,1.0),'width':2}, 'brush':(255,255,255,255), 'symbol':'s'} )
+        self.pmtdiagram = pg.ScatterPlotItem(pxMode=False)
+        self.pmtdiagram.addPoints( self.pmtspot )
+        self.diagram.addItem( self.pmtdiagram ) 
+
+
+        
--- tests/example_plot_wf.py
+++ tests/example_plot_wf.py
@@ -8,7 +8,9 @@ from pylard.pylardata.wfopdata import WFOpData
 app = QtGui.QApplication([])
 
 #  expects 'raw_wf_tree'
-fname='/Users/twongjirad/working/uboone/data/FlasherData_080115/wf_run005.root'
+#fname='/Users/twongjirad/working/uboone/data/FlasherData_080115/wf_run002.root'
+fname='/Users/twongjirad/working/uboone/data/FlasherData_080715/wf_run004.root'
+#fname='/Users/twongjirad/working/uboone/data/FlasherData_080115/wf_run005.root'
 
 opdata = WFOpData( fname )
 
