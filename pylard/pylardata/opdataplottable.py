388ce7d10ba8c8ea8be56ee54c35502236b09d1f
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-10-27 01:19:16 -0400

    defining abstract base class for data interface

--- pylard/pylardata/opdataplottable.py
+++ pylard/pylardata/opdataplottable.py
@@ -1,10 +1,26 @@
 import numpy as np
+import abc
 
 class OpDataPlottable(object):
+    __metaclass__ = abs.ABCMeta
     def __init__(self):
-        pass
+        self.beamwindows = CosmicDiscVector()
+        self.cosmicwindows = CosmicDiscVector()
 
-    def getData(self):
+    @abc.abstractmethod    
+    def gotoEvent(self, event, run, subrun ):
+        """ user must implement this. it instructs that the opdata should get the data for event, run, subrun """
         raise RuntimeError('need to overwrite this function')
-        return None
+    
+    @abs.abstracmethod:
+    def getNextEntryID( self ):
+        """ user should fill self.run, self.subrun, self.event"""
+        raise RuntimeError('need to overwrite this function')
+
+    def clearEvent( self ):
+        self.beamwindows.clear()
+        self.cosmicwindows.clear()
         
+    def getWaveformPlotData( self, tstart, tend, remake_cache=False ):
+        """ loop through beam, cosmic and user data to be plotted on the waveform canvas.  make plots, return."""
+        pass
