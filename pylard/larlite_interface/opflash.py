4c235af573d38ff059afcd6b2b66614ee16eb851
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-10-27 01:09:14 -0400

    add class to be used to store data ready to be plotted

---
+++ pylard/larlite_interface/opflash.py
@@ -0,0 +1,75 @@
+from pylard.pylardata.opdataplottable import OpDataPlottable
+import numpy as np
+import ROOT
+from ROOT import larlite
+import time
+
+class OpFlashData( OpDataPlottable ):
+
+    def __init__(self,producer='opflash'):
+        super(OpFlashData, self).__init__()
+
+        # get the producer name
+        self.producer = producer
+
+        # allow only the 32 "regular" PMTs
+        self.pmt_max = 31
+        
+        # larlite OpHit stored here:
+        self.opflashdata = None
+
+        # keep track of flashes
+        # flashes saved in per time-range dictionary
+        self.flashes = {}
+
+
+    #---------------------
+    # define producer name
+    def setProducer(self,producer):
+        self.producer = producer
+
+
+    # -------------------------
+    # return data
+    def getData(self, remake=False):
+        # if we are to clear the event
+        if remake == True:
+            self.flashes = {}
+            return self.flashes
+        # otherwise save the current hits
+        return self.flashes
+
+
+    #---------------------------
+    # get data for current event
+    def getEvent(self, mgr, trig_time=None):
+
+        self.flashes = {}
+
+        # load optical hits
+        self.opflashdata = mgr.get_data(larlite.data.kOpFlash,
+                                        self.producer)
+
+        # load each flash
+        try:
+            print 'found %i flashes'%self.opflashdata.size()
+
+            for n in xrange(self.opflashdata.size()):
+
+                flash = self.opflashdata.at(n)
+
+                # flash time
+                time = flash.Time()
+                abstime = flash.AbsTime()
+                frame = flash.Frame()
+                dt   = flash.TimeWidth()
+                PE   = flash.TotalPE()
+                Ypos = flash.YCenter()
+                Zpos = flash.ZCenter()
+                #print '\t[Time, dt, Frame]  : [%.02f, %.02f, %.02f]'%(time,dt,frame)
+                #print '\t[PE, Ypos, Zpos] : [%.02f, %.02f, %.02f]'%(PE,Ypos,Zpos)
+                #print
+                #if (PE < 20): continue
+                self.flashes[ (time, time+dt) ] = [ PE, Ypos, Zpos ]
+        except:
+            print 'could not load flashes'
---
+++ pylard/pylardata/opwfmplot.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class OpWfmPlot:
+    def __init__( self, wfm, times, slot, ch, default_color=(255,255,255,255), highlighted_color=(0,255,255,255), timepertick=None ):
+        """
+        class storing cosmic discriminator window data
+        
+        inputs:
+        ------
+        wfm: numpy array storing ADC counts
+        ch: FEMCH number
+        time: time of event -- ambiguous, let user decide what time means. can pass float or pass array
+        default_color: vector for pyqtgraph to draw color
+        highlighted_color: color used when object is clicked on
+        """
+        if type(self.times) is float and timepertick is None:
+            raise ValueError( "if setting time using a float, need to set the time per tick" )
+        self.wfm = wfm
+        self.times = times
+        self.timepertick = timepertick
+        self.slot = slot
+        self.ch = ch
+        self.default_color = default_color
+        self.highlighted_color = highlighted_color
+
+    def genTimeArray( self ):
+        """ return numpy array of times. we do this, so the time array is only allocated when needed. """
+        if type(self.times) is float:
+            if self.timepertick is not None:
+                return np.linspace( self.times, self.times+self.timepertick*len(self.wfm), len(self.wfm) )
+        return self.times
+    def getTimestamp( self ):
+        """ return time stamp, which for arrays is the first entry """
+        if type(self.times) is float:
+            return self.times
+        else:
+            return self.times[0]
+
+
+class OpWfmPlotVector:
+    def __init__(self):
+        self.chtimes = {}    # key=channel, value=list of times
+        self.chwindows = {}  # key=channel, value=dict of (timestamp,window)
+
+    def getWindowsBetweenTimes( self, start, end ):
+        out = []
+        for ch,times in self.chtimes.items():
+            times.sort()
+            t = np.asarray( times ) # we use an array to have access to numpy magic
+            tselect = t[ np.where( (t>=start) & (t<=end) ) ]
+
+            for n in range(0,len(tselect)):
+                out.append( self.chwindows[ch][ tselect[n] ] )
+        return out
+
+    def addWindow( self, cdw ):
+        if cdw.ch not in self.chtimes:
+            self.chtimes[cdw.ch] = []
+            self.chwindows[cdw.ch] = {}
+        self.chtimes[cdw.ch].append( cdw.getTimestamp() )
+        self.chwindows[ cdw.ch ][ cdw.getTimestamp() ] = cdw
+
+    def makeWindow( self, wfm, times, slot, ch, default_color=(255,255,255,255), highlighted_color=(0,255,255,255), timepertick=None ):
+        """ pass through function to OpWfmPlot constructor """
+        self.addWindow( wfm, times, slot, ch, default_color=default_color, highlighted_color=highlighted_color, timepertick=timepertick )
+            
+        
+    def getNumWindows( self ):
+        n = 0
+        for ch,times in self.chtimes.items():
+            n += len(times)
+        return n
