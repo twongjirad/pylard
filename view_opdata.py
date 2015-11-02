85c4fccdcef3a8b0eea2fde37ef7df9269bb9a60
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-11-02 11:47:06 -0600

    save viewer

--- view_opdata.py
+++ view_opdata.py
@@ -5,20 +5,19 @@ import numpy as np
 
 # pylard
 from pylard.pylardisplay.opdetdisplay import OpDetDisplay
-from pylard.pylardata.wfopdata import WFOpData
 from pylard.pylardata.rawdigitsopdata import RawDigitsOpData
 
 
-# using output of LArLite wftree
-#fname="/Users/twongjirad/working/uboone/data/20150909_CosmicDiscTuning/wf_run007.root"
-#opdata = WFOpData( fname )
-
-# using output of larsfot's RawData/util/RawDigitsWriter: 'raw_wf_tree'
-fname='/Users/twongjirad/working/uboone/data/pmtratedata/run2499_filterreconnect_subrun1.root'
+fname = sys.argv[1]
 
 opdata = RawDigitsOpData( fname )
 
 app = QtGui.QApplication([])
 opdisplay = OpDetDisplay( opdata )
 opdisplay.show()
+
+if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    print "exec called ..."
+    opdisplay.show()
+    QtGui.QApplication.instance().exec_()
 
