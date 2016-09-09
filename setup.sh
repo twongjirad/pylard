2859a51d4301772fb8ec269ad0317331c1fba67c
Taritree Wongjirad <twongjirad@MacBook-Air.local>
2016-09-09 14:00:23 -0400

    clean up

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-export PYTHONPATH=${PYTHONPATH}:/Users/twongjirad/working/uboone/pylard
+MY_LARLITE=/Users/twongjirad/working/uboone/larlite
+MY_LARCV=/Users/twongjirad/working/larbys/LArCV
+
+cd $MY_LARLITE/config
+source setup.sh
+cd -
+source $MY_LARCV/configure.sh
+source env/bin/activate
