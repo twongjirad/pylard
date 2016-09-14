9535610b415568c8e2b97488cc80b4b62f0f9459
Taritree Wongjirad <twongjirad@MacBook-Air.local>
2016-09-14 01:10:31 -0400

    added other pieces for opdata

---
+++ pylard/vislarlite/drawopdata.py
@@ -0,0 +1,18 @@
+import os,sys
+from pylard.vislarlite.larliteopdata import 
+
+
+class PyLArLiteDrawOpdata:
+    def __init__(self):
+        print "PyLArLiteDrawOpdata init"
+    def configure(self,pset):
+        print pset
+    def visualize( self, larlite_io, larcv_io ):
+        """ fill larliteopdata object. its a concrete instance of opdataplottable """
+        print "Visualize PyLArLiteDrawOpdata"
+        print " larlite_io: ",larlite_io
+        print " larcv_io: ",larcv_io
+
+        opdata = 
+        
+        
